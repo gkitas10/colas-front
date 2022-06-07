51858cbdc8bb27549ddac389a6803a98755a609c
Jorge <gkitas10@gmail.com>
2022-06-07 16:48:37 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import socket from './Socket';
+import './App.css';
+
+function App() {
+  useEffect(() => {
+    console.log('jaja')
+  }, [])
+  
+
+  return (
+    <div className="App">
+      app
+    </div>
+  );
+}
+
+export default App;
