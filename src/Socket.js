8bd96e7838fe51f209ca4b91c35dd864ef1b637f
unknown <gkitas10@gmail.com>
2022-06-08 15:42:11 -0500

    Add backend production url

--- src/Socket.js
+++ src/Socket.js
@@ -1,5 +1,6 @@
 import io from 'socket.io-client';
+const url = process.env.NODE_ENV === 'production' ? 'https://colas-back.herokuapp.com/' : 'http://localhost:4000'
 
-let socket = io('http://localhost:4000');
+let socket = io(url);
 
 export default socket;
