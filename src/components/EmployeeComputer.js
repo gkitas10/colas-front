782302e8aa87f0a8637b77e2d1ff2b61d669a5ed
unknown <gkitas10@gmail.com>
2022-06-15 18:10:06 -0500

    Style EmployeeComputer

--- src/components/EmployeeComputer.js
+++ src/components/EmployeeComputer.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import { useSearchParams } from 'react-router-dom';
+import styles from '../styles/EmployeeComputer.module.css';
 import socket from '../Socket';
 
 const EmployeeComputer = () => {
-  
     const [ ticketassigned, setTicketassigned ] = useState('');
     const [ ticketqueue, setTicketqueue ] =useState('')
     const [searchParams] = useSearchParams();
     const computer = (Object.fromEntries([...searchParams]));
-
+console.log(computer);
     socket.on('ticket-queue', function(data) {
       console.log(data);
       setTicketqueue(data)
@@ -35,14 +35,17 @@ const EmployeeComputer = () => {
   
 
   return (
-    <div>
-        <form onSubmit={handleSubmit}>
+    <div className={styles.main}>
+      <div className={styles.info_cont}>
         <h1>Escritorio</h1>
-        <h4>Atendiendo a <small>{ ticketassigned ? 'Ticket ' + ticketassigned : '...'}</small></h4>
-        <div>{ ticketqueue }</div>
-        <input type='submit' value='Atender siguiente ticket'/>
+        <div>Atendiendo a <small>{ ticketassigned ? 'Ticket ' + ticketassigned : '...'}</small></div>
+        <div className={styles.pending_tickets}>Tickets por atender { ticketqueue }</div>
+      </div>
+      <div className={styles.form_cont}>
+        <form onSubmit={handleSubmit}>
+          <input type='submit' value='Atender siguiente ticket' className={styles.input_submit}/>
         </form>
-        
+      </div>
     </div>
   )
 }
