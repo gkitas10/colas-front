103c5914d7b0957ac9ef82bc4f88f8e2ccd4dd5f
unknown <gkitas10@gmail.com>
2022-06-15 18:12:12 -0500

    Add style to EmployeeInterface

--- src/components/EmployeeInterface.js
+++ src/components/EmployeeInterface.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import styles from '../styles/EmployeeInterface.module.css';
 
 const EmployeeInterface = () => {
     const [ computer , setComputer ] = useState(''); 
@@ -10,24 +11,30 @@ const EmployeeInterface = () => {
     }
 
   return (
-    <div>
-        <div>
-            <div className="">
-                <Link to='/public-screen'>Pantalla publica</Link>
-                <Link to='/create-ticket'>Crear Tickets</Link>
+    <div className={styles.main}>
+        <div className={styles.cont}>
+            <div className={styles.title}>
+                Interfaz de empleado
+            </div>
+            <div className={styles.links_row}>
+                <div className={styles.links_cont}>
+                    <Link to='/public-screen' className={styles.link}>Pantalla publica</Link>
+                    <Link to='/create-ticket' className={styles.link+ ' ' + styles.link_gray}><a>Crear Tickets</a></Link>
+                </div>
+            </div>
+            <div className={styles.form_cont}>
+                <form onSubmit={handleSubmit} className={styles.form}>
+                    <input name="computer" type="number" className={styles.input_num} placeholder="Escritorio" onChange={e => setComputer(e.target.value)}/>
+                    <button className={styles.btn}>
+                        Ingresar
+                    </button>
+                </form>
+            </div>
+            <div>
+            { alert }
             </div>
         </div>
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input name="computer" type="number" className="form-control" placeholder="Escritorio" autoFocus required onChange={e => setComputer(e.target.value)}/>
-                <button>
-                    Ingresar
-                </button>
-            </form>
-        </div>
-        <div>
-           { alert }
-        </div>
+        
     </div>
 )
 }
