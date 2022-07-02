95b2e452996e902ebba5903710e3f92143a7315e
unknown <gkitas10@gmail.com>
2022-07-01 20:42:40 -0500

    Update caniuse and change backend url

--- src/Socket.js
+++ src/Socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
-const url = process.env.NODE_ENV === 'production' ? 'https://colas-back.herokuapp.com/' : 'http://localhost:4000'
+const url = process.env.NODE_ENV === 'production' ? 'https://queuesapp-back.herokuapp.com/' : 'http://localhost:4000'
 
 let socket = io(url);
 
