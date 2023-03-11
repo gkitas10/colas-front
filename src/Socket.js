9461ffaa9a147cd4a1919bb996439f0a664c455a
unknown <gkitas10@gmail.com>
2023-03-11 14:30:31 -0600

    Change backend URI

--- src/Socket.js
+++ src/Socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
-const url = process.env.NODE_ENV === 'production' ? 'https://queuesapp-back.herokuapp.com/' : 'http://localhost:4000'
+const url = process.env.NODE_ENV === 'production' ? 'https://queues-back.onrender.com/' : 'http://localhost:4000'
 
 let socket = io(url);
 
